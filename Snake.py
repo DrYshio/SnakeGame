976d11b1317ce71efda3ba6a3c60fa929570db31
DrYshio <maksim.tarhov@mail.ru>
2020-10-18 14:19:29 +0300

    updated Snake class

--- Snake.py
+++ Snake.py
@@ -20,3 +20,27 @@ class Snake:
     def draw_snake(self, surf):
         for segment in self.body:
             pygame.draw.rect(surf, pygame.Color('Green'), pygame.Rect(segment[0], segment[1], 10, 10))
+
+    def animation(self):
+        self.body.insert(0, list(self.head))
+        self.body.pop()
+
+    def check_board(self):
+        if self.head[0] == 649:
+            self.head[0] = 11
+        elif self.head[0] == 0:
+            self.head[0] = 638
+        elif self.head[1] == 0:
+            self.head[1] = 451
+        elif self.head[1] == 462:
+            self.head[1] = 11
+
+    def eat(self, food):
+        if self.head == food.food_position:
+            self.body.append(food.food_position)
+            food.get_food_position()
+            self.score += 1
+
+    def check_death(self):
+        if self.head in self.body:
+            self.score = 0
